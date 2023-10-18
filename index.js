7b4762ac7a3573be14bc161be1349f6946416923
Samson Githinji <githinjisamson148@gmail.com>
2023-10-18 08:07:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize(string) {
+    // words to remain in small caps
+    const exceptWords = [
+      "a",
+      "an",
+      "the",
+      "and",
+      "for",
+      "but",
+      "of",
+      "at",
+      "by",
+      "from",
+    ];
+
+    // each letter to be an array item
+    const splitString = string.split(" ");
+
+    // empty to put final
+    const titleized = [];
+
+    // iterate splitString
+    for (let i = 0; i < splitString.length; i++) {
+      if (i === 0 || !exceptWords.includes(splitString[i].toLowerCase())) {
+        // none of the letters in exceptWords
+        titleized.push(
+          splitString[i].charAt(0).toUpperCase() +
+            splitString[i].slice(1).toLowerCase()
+        );
+      } else {
+        // letter in exceptWords
+        titleized.push(splitString[i].toLowerCase());
+      }
+    }
+    return titleized.join(" ");
+  }
 }
